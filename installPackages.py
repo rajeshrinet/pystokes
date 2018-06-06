cc95ecdded5a4e42ec0dfdfa6c11d2a0854c97f0
Rajesh Singh <rajeshrinet@gmail.com>
2018-06-06 12:03:54 +0100

    minor changes

--- installPackages.py
+++ installPackages.py
@@ -3,15 +3,15 @@ import os
 owd = os.getcwd()
 
 os.chdir('pystokes/')
-os.system('python setup.py install')
+os.system('/srv/conda/bin/python setup.py install')
 
 os.chdir(owd)
 os.chdir('pyforces/')
-os.system('python setup.py install')
+os.system('/srv/conda/bin/python setup.py install')
 
 os.chdir(owd)
 os.chdir('examples/unbounded/activeColloidsLatticeTraps/')
-os.system('python setup.py install')
+os.system('/srv/conda/bin/python setup.py install')
 
 os.chdir(owd)
 

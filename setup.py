21c24044656817971dfdb42b139daf7c2ad48a0b
Rajesh Singh <rajeshrinet@gmail.com>
2020-06-27 13:32:14 +0100

    minor

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pystokes',
-    version='2.1.8',
+    version='2.1.10',
     url='https://github.com/rajeshrinet/pystokes',
     author = 'The PyStokes team',
     author_email = 'PyStokes@googlegroups.com',
